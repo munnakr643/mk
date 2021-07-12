8e670518f8e212cbec54d7575380ff09b0b47bea
munna <munna@ensuredit.com>
2021-07-12 22:12:22 +0530

    Update BaseTest.java

--- src/test/java/webqa/ensurity/base/BaseTest.java
+++ src/test/java/webqa/ensurity/base/BaseTest.java
@@ -251,8 +251,7 @@ public class BaseTest {
 	
 
 	public void enterOtp()  {
-		waitFor(1);
-//		waitElement(driver.findElement(By.xpath("//span[text()='Proceed']")), 7);
+		waitElement(loginPage.otpbox1, 7);
 		assertThat(loginPage.otpbox1.isDisplayed(), equalTo(true));
 		String otp=readconfig.getPassword();
 		for(int i=1;i<=otp.length();i++){
